34416333abf9158cf175129ab9ad610ee3970451
Komal Choubey <komalchaubey2020@gmail.com>
2026-05-14 19:02:28 +0530

    fixed requriments

--- tripwise_backend/tripwise_backend/settings.py
+++ tripwise_backend/tripwise_backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g(l*&)n(x4%wz8n*mj7rv=h9h8zr29d95f45ox-a4+yo95(#k)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
