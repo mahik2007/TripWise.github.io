566acc225621df651f21676db722ec2343f4ba92
Mansi <mansikashyap134@gmail.com>
2026-05-15 11:18:33 +0530

    debugging

--- tripwise_backend/api/views.py
+++ tripwise_backend/api/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.contrib.auth.hashers import make_password, check_password
 from .models import *
+
+
+def home(request):
+    return HttpResponse("Backend Running Successfully")
+
 
 # Signup
 @api_view(['POST'])
